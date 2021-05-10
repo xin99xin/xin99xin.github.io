fc7ae0cbe2b79cb0910c78e231098f92749e953c
zodiac <xin99xin@gmail.com>
2021-05-10 20:13:56 +0800

    enable-eslint

--- src/components/ShowModel.vue
+++ src/components/ShowModel.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script>
-/* eslint-disable */
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
@@ -13,17 +12,17 @@ import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
 
 export default {
   name: 'ShowModel',
-  data(){
+  data () {
     return {
       reData: new Array(this.weeks),
       fileName: null,
-      userId:null,
-      userName:null,
-      year:null,
+      userId: null,
+      userName: null,
+      year: null,
       weeks: 52,
       gender: null,
 
-      //模型相关
+      // 模型相关
       cellGroup: null,
       baseGroup: null,
       camera: null,
@@ -37,35 +36,35 @@ export default {
       Width: null,
       height: null,
 
-      //单元格属性
-      cell:{
-        maxX: 11,//最大列数
-        maxY: 5,//最大行数
-        sideLength: 15,//单元格边长
-        padding: 1,//边缘填充
+      // 单元格属性
+      cell: {
+        maxX: 11, // 最大列数
+        maxY: 5, // 最大行数
+        sideLength: 15, // 单元格边长
+        padding: 1, // 边缘填充
         material: new THREE.MeshStandardMaterial({
-          color: 0x005691,
-        }),
+          color: 0x005691
+        })
       }
 
     }
   },
   mounted () {
-    this.width = document.getElementById('container').clientWidth;
-    this.height = document.getElementById('container').clientHeight;
-    this.cellGroup = new THREE.Group();
-    this.cellGroup.position.set(0,0,0);
-    this.baseGroup = new THREE.Group();
-    this.baseGroup.position.set(0,0,0);
+    this.width = document.getElementById('container').clientWidth
+    this.height = document.getElementById('container').clientHeight
+    this.cellGroup = new THREE.Group()
+    this.cellGroup.position.set(0, 0, 0)
+    this.baseGroup = new THREE.Group()
+    this.baseGroup.position.set(0, 0, 0)
     this.init()
   },
 
-  methods:{
-    //初始化模型
-    init(){
-      this.getReData();
+  methods: {
+    // 初始化模型
+    init () {
+      this.getReData()
       this.createScene() // 创建场景
-      this.createModelColumns();
+      this.createModelColumns()
       this.loadMesh() // 载入底座模型
       this.createLight() // 创建光源
       this.createCamera() // 创建相机
@@ -75,51 +74,51 @@ export default {
       window.onresize = this.onWindowResize
     },
 
-    //获取数据库查询结果
-    getReData() {
-      //生成测试数据
-      for (var index = 0 ; index<=this.weeks;index++){
-        this.reData[index] = {"weekNum": index+1,"hours": Math.round(Math.random()*30)};
+    // 获取数据库查询结果
+    getReData () {
+      // 生成测试数据
+      for (var index = 0; index <= this.weeks; index++) {
+        this.reData[index] = {'weekNum': index + 1, 'hours': Math.round(Math.random() * 30)}
       }
-      console.log(this.reData);
+      console.log(this.reData)
 
-      //TODO ajax
+      // TODO ajax
     },
 
-    //创建柱状体
-    createModelColumns(){
-      var boxSize = this.cell.sideLength-this.cell.padding*2;
-      var geometry = new THREE.BoxGeometry(boxSize,boxSize,1);
-      var x,y //当前单元格坐标
-      for(var row of this.reData){
-        var mesh = new THREE.Mesh(geometry,this.cell.material);
-        mesh.scale.set(1,1,row.hours*1.4);//加高一些
+    // 创建柱状体
+    createModelColumns () {
+      var boxSize = this.cell.sideLength - this.cell.padding * 2
+      var geometry = new THREE.BoxGeometry(boxSize, boxSize, 1)
+      var x, y // 当前单元格坐标
+      for (var row of this.reData) {
+        var mesh = new THREE.Mesh(geometry, this.cell.material)
+        mesh.scale.set(1, 1, row.hours * 1.4)// 加高一些
 
-        //根据weekNum计算当前的单元格坐标
-        x = Math.floor((row.weekNum-1) / this.cell.maxY);//星期序号除最大行数向下取整
-        y = (row.weekNum -1 + this.cell.maxY) % this.cell.maxY;//取模
-        var pX = x * this.cell.sideLength + this.cell.padding;
-        var pY = y * this.cell.sideLength + this.cell.padding;
+        // 根据weekNum计算当前的单元格坐标
+        x = Math.floor((row.weekNum - 1) / this.cell.maxY)// 星期序号除最大行数向下取整
+        y = (row.weekNum - 1 + this.cell.maxY) % this.cell.maxY// 取模
+        var pX = x * this.cell.sideLength + this.cell.padding
+        var pY = y * this.cell.sideLength + this.cell.padding
 
-        //由于高度有拉伸，需要调整z轴坐标
-        mesh.position.set(pX,pY,row.hours*0.7);
-        this.cellGroup.add(mesh);
+        // 由于高度有拉伸，需要调整z轴坐标
+        mesh.position.set(pX, pY, row.hours * 0.7)
+        this.cellGroup.add(mesh)
       }
     },
 
-    createScene(){
+    createScene () {
       this.scene = new THREE.Scene({
         background: new THREE.Color(0x005691)
-      }).add(new THREE.AxisHelper());
-      this.scene.add(this.cellGroup).add(this.baseGroup);
+      }).add(new THREE.AxisHelper())
+      this.scene.add(this.cellGroup).add(this.baseGroup)
     },
-    loadMesh(){
+    loadMesh () {
       new OBJLoader().load('static/shapr3d_export_2021-05-09_12h29m.obj', (obj) => {
         var mesh = obj.children[0]
         mesh.geometry.center()
         mesh.scale.set(1000, 1000, 1000)
         mesh.material = new THREE.MeshBasicMaterial({
-          color: 0xffff00,
+          color: 0xffff00
         })
         this.baseGroup.add(mesh)
       })
@@ -145,10 +144,9 @@ export default {
     },
     // 创建渲染器
     createRender () {
-
       this.renderer = new THREE.WebGLRenderer({
         antialias: true,
-        alpha: true,
+        alpha: true
       })
       this.renderer.setSize(this.width, this.height) // 设置渲染区域尺寸
       document.getElementById('container').appendChild(this.renderer.domElement)
@@ -158,7 +156,7 @@ export default {
       requestAnimationFrame(this.render)
       this.dirLight.position.copy(this.camera.position)
       this.pointLight.position.copy(this.camera.position)
-      this.controls.update();
+      this.controls.update()
       this.renderer.render(this.scene, this.camera)
     },
 
@@ -168,25 +166,23 @@ export default {
       this.controls.enableZoom = true
       this.controls.minDistance = 100
       this.controls.maxDistance = 800
-      this.controls.autoRotate = true;
-      this.controls.autoRotateSpeed = 1.0; // 30 seconds per round when fps is 60
-      this.controls.enableDamping = true;
-      this.controls.dampingFactor = 0.8;
+      this.controls.autoRotate = true
+      this.controls.autoRotateSpeed = 1.0 // 30 seconds per round when fps is 60
+      this.controls.enableDamping = true
+      this.controls.dampingFactor = 0.8
     },
 
-    //窗口变动触发的函数
+    // 窗口变动触发的函数
     onWindowResize () {
-      this.width = document.getElementById('container').clientWidth;
-      this.height = document.getElementById('container').clientHeight;
-      this.renderer.setSize(this.width,this.height);
-      this.camera.aspect = this.width / this.height;
+      this.width = document.getElementById('container').clientWidth
+      this.height = document.getElementById('container').clientHeight
+      this.renderer.setSize(this.width, this.height)
+      this.camera.aspect = this.width / this.height
       this.camera.updateProjectionMatrix()
-      this.renderer.render(this.scene, this.camera);
+      this.renderer.render(this.scene, this.camera)
     }
 
   }
-
-
 
 }
 </script>
