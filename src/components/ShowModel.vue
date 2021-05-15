525fde90ea3cf6bcb7de2d53803e8a829acd7d3e
zodiac <xin99xin@gmail.com>
2021-05-15 12:24:51 +0800

    发布测试

--- src/components/ShowModel.vue
+++ src/components/ShowModel.vue
@@ -4,8 +4,8 @@
 
 <script>
 import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 // import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 // import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 // import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
@@ -14,21 +14,25 @@ export default {
   name: 'ShowModel',
   data () {
     return {
-      // 用户信息
-      userinfo: {
-        userId: 0,
-        userName: '',
-        year: '',
-        gender: ''
-      },
+      reData: new Array(this.weeks),
+      fileName: null,
+      userId: null,
+      userName: null,
+      year: null,
+      weeks: 52,
+      gender: null,
 
       // 模型相关
       modelUrl: 'static/shapr3d_export_2021-05-09_12h29m.obj',
-      reData: [],
       rootGroup: new THREE.Group(),
+      lights: {
+        dirLight: new THREE.Light(),
+        pointLight: new THREE.Light()
+      },
       camera: null,
-      scene: new THREE.Scene(),
-      renderer: new THREE.WebGLRenderer(),
+      scene: null,
+      renderer: null,
+      controls: null,
       colors: {
         A: {
           M: 0x0969a2,
@@ -47,40 +51,36 @@ export default {
           R: 0xFFC073
         }
       },
+
+      Width: null,
+      height: null,
+
+      // 单元格属性
       cell: {
         maxX: 11, // 最大列数
         maxY: 5, // 最大行数
         sideLength: 15, // 单元格边长
         padding: 1 // 边缘填充
-      },
-      STLFileName: '',
-
-      // 页面、容器、目标旋转、鼠标位置等
-      Width: null,
-      height: null,
-      container: null,
-      targetRotation: 0,
-      targetRotationOnPointerDown: 0,
-      pointerX: 0,
-      pointerXOnPointerDown: 0
+      }
     }
   },
   mounted () {
     this.width = document.getElementById('container').clientWidth
     this.height = document.getElementById('container').clientHeight
-    this.scene = new THREE.Scene().add(this.rootGroup)
+    this.cellGroup = new THREE.Group()
+    this.baseGroup = new THREE.Group()
+    this.rootGroup.add(this.cellGroup, this.baseGroup)
+    this.scene = new THREE.Scene()
+      .add(new THREE.AxisHelper())
+      .add(this.rootGroup)
+      .add(this.lights.dirLight, this.lights.pointLight)
     this.getReData()
-    this.createColumns(this.reData)
-    this.loadBase(this.modelUrl, 1000)
+    this.createModelColumns()
+    this.loadMesh() // 载入底座模型
     this.createLight() // 创建光源
     this.createCamera() // 创建相机
-    this.renderer.setSize(this.width, this.height) // 设置渲染区域尺寸
-    document.getElementById('container').appendChild(this.renderer.domElement)
-    this.container = document.getElementById('container')
-    this.container.appendChild(this.renderer.domElement)
-    this.container.style.touchAction = 'none'
-    this.container.addEventListener('pointerdown', this.onPointerDown)
-    this.createControls()
+    this.createRender() // 创建渲染器
+    this.createControls() // 创建控件对象
     this.render() // 渲染
     window.onresize = this.onWindowResize
   },
@@ -89,92 +89,99 @@ export default {
     // 获取数据库查询结果
     getReData () {
       // 生成测试数据
-      for (let index = 0; index <= 51; index++) {
+      for (var index = 0; index <= this.weeks; index++) {
         this.reData[index] = {'weekNum': index + 1, 'hours': Math.round(Math.random() * 30)}
       }
+      console.log(this.reData)
 
       // TODO ajax
     },
 
     // 创建柱状体
-    createColumns (data) {
-      const boxSize = this.cell.sideLength - this.cell.padding * 2
-      const geometry = new THREE.BoxGeometry(boxSize, boxSize, 1)
-      const group = new THREE.Group()
-      let x, y // 当前单元格坐标
-      for (const row of data) {
-        const mesh = new THREE.Mesh(
-          geometry,
-          new THREE.MeshBasicMaterial({
-            color: this.colors.A.M,
-            wireframe: true
-          })
-        )
+    createModelColumns () {
+      var boxSize = this.cell.sideLength - this.cell.padding * 2
+      var geometry = new THREE.BoxGeometry(boxSize, boxSize, 1)
+      var material = new THREE.MeshBasicMaterial({
+        color: this.colors.B.M,
+        wireframe: true
+      })
+      var x, y // 当前单元格坐标
+      for (var row of this.reData) {
+        var mesh = new THREE.Mesh(geometry, material)
         mesh.scale.set(1, 1, row.hours * 1.4)// 加高一些
 
         // 根据weekNum计算当前的单元格坐标
         x = Math.floor((row.weekNum - 1) / this.cell.maxY)// 星期序号除最大行数向下取整
         y = (row.weekNum - 1 + this.cell.maxY) % this.cell.maxY// 取模
-        const pX = x * this.cell.sideLength + this.cell.padding
-        const pY = y * this.cell.sideLength + this.cell.padding
+        var pX = x * this.cell.sideLength + this.cell.padding
+        var pY = y * this.cell.sideLength + this.cell.padding
 
         // 由于高度有拉伸，需要调整z轴坐标
         mesh.position.set(pX, pY, row.hours * 0.7)
-        mesh.name = row.weekNum + ':' + row.hours
-        group.add(mesh)
+        this.cellGroup.add(mesh)
       }
-      group.name = 'columns'
-      this.setCenter(group)
-      // group.position.set(-center.x, -center.y, -center.z)
-      this.rootGroup.add(group)
+      this.scene.add(this.cellGroup)
     },
 
-    loadBase (url, scale) {
-      new OBJLoader().load(url, (obj) => {
-        obj.scale.set(scale, scale, scale)
-        obj.children[0].material = new THREE.MeshBasicMaterial({
-          color: this.colors.A.M,
-          wireframe: true}
-        )
-        obj.name = 'base'
-        this.setCenter(obj)
-        this.rootGroup.add(obj)
-        // 根据base和columns的size调整columns的z轴位置
-        const objSize = this.getSize(obj)
-        const columns = this.rootGroup.getObjectByName('columns')
-        const columnsSize = this.getSize(columns)
-        columns.translateZ((objSize.z + columnsSize.z) / 2)
-        this.rootGroup.lookAt(0, 1, 0)
+    loadMesh () {
+      new OBJLoader().load(this.modelUrl, (obj) => {
+        var mesh = obj.children[0]
+        mesh.geometry.center()
+        mesh.scale.set(1000, 1000, 1000)
+        mesh.material = new THREE.MeshBasicMaterial({
+          color: this.colors.D.R,
+          wireframe: true
+        })
+        this.baseGroup.add(mesh)
+        this.createModelColumns()
+        var size = new THREE.Box3().expandByObject(this.baseGroup).getSize()
+        var center = new THREE.Box3().expandByObject(this.cellGroup).getCenter()
+        this.cellGroup.position = new THREE.Vector3(-center.x, -center.y, size.z / 2)
       })
+      this.scene.add(this.baseGroup)
+      this.rootGroup.rotateX(3.14 / 180 * 90)
     },
 
+    // 创建光源
     createLight () {
+      this.dirLight = new THREE.DirectionalLight(0xff0000, 0.725)
+      this.dirLight.position.set(0, 0, 1).normalize()
+      this.scene.add(this.dirLight)
+      this.pointLight = new THREE.PointLight(0xffffff, 1.5)
+      this.pointLight.position.set(0, 100, 90)
+      this.scene.add(this.pointLight)
     },
-
     // 创建相机
     createCamera () {
       const k = this.width / this.height // 窗口宽高比
-      this.camera = new THREE.PerspectiveCamera(30, k, 1, 2500)
+      this.camera = new THREE.PerspectiveCamera(30, k, 1, 1500)
       this.camera.position.set(0, 300, 700)
-      this.camera.lookAt(this.rootGroup.position) // 没有controls的情况必须设置，血的教训
+      this.camera.lookAt(new THREE.Vector3(0, 0, 0)) // 设置相机方向
       this.scene.add(this.camera)
+    },
+    // 创建渲染器
+    createRender () {
+      this.renderer = new THREE.WebGLRenderer()
+      this.renderer.setSize(this.width, this.height) // 设置渲染区域尺寸
+      document.getElementById('container').appendChild(this.renderer.domElement)
     },
 
     render () {
       requestAnimationFrame(this.render)
-      this.rootGroup.rotation.z += (this.targetRotation - this.rootGroup.rotation.z) * 0.05
-      this.renderer.clear()
+      this.controls.update()
       this.renderer.render(this.scene, this.camera)
     },
 
     // 创建控件对象
     createControls () {
-      const controls = new OrbitControls(this.camera, this.renderer.domElement)
-      controls.enableZoom = true
-      controls.minDistance = 100
-      controls.maxDistance = 2000
-      controls.enableDamping = true
-      controls.dampingFactor = 0.8
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+      this.controls.enableZoom = true
+      this.controls.minDistance = 100
+      this.controls.maxDistance = 800
+      this.controls.autoRotate = true
+      this.controls.autoRotateSpeed = 1.0 // 30 seconds per round when fps is 60
+      this.controls.enableDamping = true
+      this.controls.dampingFactor = 0.8
     },
 
     // 窗口变动触发的函数
@@ -185,39 +192,8 @@ export default {
       this.camera.aspect = this.width / this.height
       this.camera.updateProjectionMatrix()
       this.renderer.render(this.scene, this.camera)
-    },
-    // 获取物体位置和尺寸
-    setCenter (obj) {
-      const center = new THREE.Vector3()
-      new THREE.Box3().expandByObject(obj).getCenter(center)
-      obj.position.set(-center.x, -center.y, -center.z)
-    },
-    getSize (obj) {
-      const size = new THREE.Vector3()
-      new THREE.Box3().expandByObject(obj).getSize(size)
-      return size
-    },
-
-    // 控制旋转（鼠标按下、移动、抬起）
-    onPointerDown (event) {
-      if (event.isPrimary === false) return
-      this.pointerXOnPointerDown = event.clientX - this.width / 2
-      this.targetRotationOnPointerDown = this.targetRotation
-      document.addEventListener('pointermove', this.onPointerMove)
-      document.addEventListener('pointerup', this.onPointerUp)
-    },
-
-    onPointerMove (event) {
-      if (event.isPrimary === false) return
-      this.pointerX = event.clientX - this.width / 2
-      this.targetRotation = this.targetRotationOnPointerDown + (this.pointerX - this.pointerXOnPointerDown) * 0.02
-    },
-
-    onPointerUp (event) {
-      if (event.isPrimary === false) return
-      document.removeEventListener('pointermove', this.onPointerMove)
-      document.removeEventListener('pointerup', this.onPointerUp)
     }
+
   }
 
 }
